09dfb677b20eef9020aae41de3827eed543cb2e7
marslankh092 <marslankh092@gmail.com>
2023-03-29 22:54:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/upwork_rehman_fyp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.upwork_rehman_fyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, Splash.class));
+        finish();
+    }
+}
